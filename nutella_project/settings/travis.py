1b24f1337d77e740c7b09e70b1aea5527d2d6c1d
tireub <tireub@msn.com>
2018-10-02 17:03:42 +0200

    add travis files

---
+++ nutella_project/settings/travis.py
@@ -0,0 +1,13 @@
+from . import *
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': '',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+        },
+}
+
